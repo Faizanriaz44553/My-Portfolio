de4428632b5977bc4ffe72d7d94ba5bcb5afa629
Faizanriaz44553 <sylanipractice@gmail.com>
2024-04-15 05:07:11 -0700

    project update

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,1 @@
-// window.addEventListener('DOMContentLoaded', function() {
-//   var navbar = document.getElementById('navbar');
-//   var lastScrollTop = 0;
 
-//   window.addEventListener('scroll', function() {
-//       var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-//       if (scrollTop > lastScrollTop) {
-//           navbar.style.top = "-60px"; // Navbar ko hide karne ke liye negative top value set karein
-//       } else {
-//           navbar.style.top = "0"; // Navbar ko show karne ke liye top value ko wapas 0 par set karein
-//       }
-//       lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Ensure kiya jata hai ki scrollTop 0 se kam na ho
-//   });
-// });
