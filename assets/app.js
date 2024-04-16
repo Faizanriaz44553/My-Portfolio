7f6af794a8a6a1b2d84157263a50538087752100
Faizanriaz44553 <sylanipractice@gmail.com>
2024-04-16 04:02:01 -0700

    projet initialization

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,12 @@
+// const num = prompt('enter number')
+// const num2 = prompt('enter number')
 
+
+// console.log = num + num2;
+
+
+// const result = document.querySelector ("#result")
+// function work() {
+//     console.log (" ")
+//     console.log ("hell world");
+// }
