3313207361b9b6af7b9b283b94de0baec11c8369
Faizanriaz44553 <sylanipractice@gmail.com>
2025-01-15 02:57:23 -0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   resolve: {
-    alias: mode === "production" ? { "@babel/runtime": require.resolve("@babel/runtime") } : {},
+    alias: mode === "production" ? { "@babel/runtime": require.resolve("/node_modules/@babel/runtime") } : {},
   },
 }));
+
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   resolve: {
+//     alias: {
+//       '@babel/runtime': '/node_modules/@babel/runtime',
+//     },
+//   },
+// });
