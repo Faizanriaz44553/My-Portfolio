b9753c9d6b19cf27bd0889decbecb6234de70ed1
Faizanriaz44553 <sylanipractice@gmail.com>
2025-01-15 02:59:19 -0800

    project initialization

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  resolve: {
-    alias: mode === "production" ? { "@babel/runtime": require.resolve("/node_modules/@babel/runtime") } : {},
-  },
-}));
-
-// import { defineConfig } from 'vite';
-
+// // https://vite.dev/config/
 // export default defineConfig({
-//   resolve: {
-//     alias: {
-//       '@babel/runtime': '/node_modules/@babel/runtime',
-//     },
-//   },
-// });
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@babel/runtime': '/node_modules/@babel/runtime',
+    },
+  },
+});
